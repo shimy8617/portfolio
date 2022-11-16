df8021ab715c0f7cc425637ec6ebf9f191f5572c
shimy8617 <cristinashim@yahoo.com.ar>
2022-11-15 23:15:10 -0300

    fontawesome

--- src/routes/contact/contact.jsx
+++ src/routes/contact/contact.jsx
@@ -1,26 +1,19 @@
-import { createRoot } from 'react-dom/client';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
 
-const element = <FontAwesomeIcon icon={faCoffee} />;
+import { Container } from "react/boostrap";
 
-createRoot.render(element, document.body);
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { brands } from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
 
 export const Contact = () => {
-    return(
-        <div className='container'>
-            <button type="button" className="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Tooltip on top">
-            <i className="fa-brands fa-linkedin"></i>
-            </button>
-            <button type="button" className="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Tooltip on right">
-                Tooltip on right
-            </button>
-            <button type="button" className="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Tooltip on bottom">
-                Tooltip on bottom
-            </button>
-            <button type="button" className="btn btn-secondary" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Tooltip on left">
-                Tooltip on left
-            </button>
+  return (
+    <Container>
+      <h1>Contact me</h1>
+      <div>
+        <a href="http://linkedin.com">
+          <FontAwesomeIcon icon={brands("linkedin")} />
+        </a>
       </div>
-    )
-}
+    </Container>
+  );
+};
