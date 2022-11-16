94bd170a5f32c7ec5e0b0b000bd12ad9a84f4835
shimy8617 <cristinashim@yahoo.com.ar>
2022-11-16 14:55:33 -0300

    contact buttons

--- src/routes/contact/contact.jsx
+++ src/routes/contact/contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { FaLinkedin, FaInstagram, FaFacebook } from 'react-icons/fa'; // <-- import styles to be used
+import { FaLinkedinIn, FaInstagram, FaAt } from 'react-icons/fa'; // <-- import styles to be used
 
 import './contact.css';
 
@@ -8,18 +8,16 @@ export const Contact = () => {
   return (
     <div className="containerContact">
         <h1 className="title"><span>Contact</span><span>me:</span></h1>
-        <div className="container text-center">
             <div className="row row-cols-1 socialMedia">
                 <div className="col">
-                <a className="btn btn-primary" href="https://www.linkedin.com/in/cristina-elisabet-shim-b5809ba8/" role="button"><FaLinkedin className="icon" /></a>
+                <a className="btn btn-primary" href="https://www.linkedin.com/in/cristina-elisabet-shim-b5809ba8/" role="button"><FaLinkedinIn className="icon" /></a>
                 </div>
                 <div className="col">
                 <a className="btn btn-primary" href="https://www.linkedin.com/in/cristina-elisabet-shim-b5809ba8/" role="button"><FaInstagram className="icon" /></a>
                 </div>
                 <div className="col">
-                <a className="btn btn-primary" href="https://www.linkedin.com/in/cristina-elisabet-shim-b5809ba8/" role="button"><FaFacebook className="icon" /></a>
+                <a className="btn btn-primary" href="https://www.linkedin.com/in/cristina-elisabet-shim-b5809ba8/" role="button"><FaAt className="icon" /></a>
                 </div>
-            </div>
         </div>
     </div>
   );
