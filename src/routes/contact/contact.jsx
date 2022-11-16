bb50d21c76cb9a57bf3e4b14dab1944221ed9c90
shimy8617 <cristinashim@yahoo.com.ar>
2022-11-16 13:19:27 -0300

    contact

--- src/routes/contact/contact.jsx
+++ src/routes/contact/contact.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 
-import { Container } from "react/boostrap";
+import { FaLinkedin, FaInstagram, FaFacebook } from 'react-icons/fa'; // <-- import styles to be used
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { brands } from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
+import './contact.css';
 
 export const Contact = () => {
   return (
-    <Container>
-      <h1>Contact me</h1>
-      <div>
-        <a href="http://linkedin.com">
-          <FontAwesomeIcon icon={brands("linkedin")} />
-        </a>
-      </div>
-    </Container>
+    <div className="containerContact">
+        <h1 className="title"><span>Contact</span><span>me:</span></h1>
+        <div className="container text-center">
+            <div className="row row-cols-1 socialMedia">
+                <div className="col">
+                <a className="btn btn-primary" href="https://www.linkedin.com/in/cristina-elisabet-shim-b5809ba8/" role="button"><FaLinkedin className="icon" /></a>
+                </div>
+                <div className="col">
+                <a className="btn btn-primary" href="https://www.linkedin.com/in/cristina-elisabet-shim-b5809ba8/" role="button"><FaInstagram className="icon" /></a>
+                </div>
+                <div className="col">
+                <a className="btn btn-primary" href="https://www.linkedin.com/in/cristina-elisabet-shim-b5809ba8/" role="button"><FaFacebook className="icon" /></a>
+                </div>
+            </div>
+        </div>
+    </div>
   );
 };
