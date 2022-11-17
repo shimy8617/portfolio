2066c9a1eef86f278d51041a2ae77e6d7077842e
shimy8617 <cristinashim@yahoo.com.ar>
2022-11-16 22:08:36 -0300

    home

--- src/routes/contact/contact.jsx
+++ src/routes/contact/contact.jsx
@@ -1,24 +1,45 @@
 import React from "react";
 
-import { FaLinkedinIn, FaInstagram, FaAt } from 'react-icons/fa'; // <-- import styles to be used
+import { FaLinkedinIn, FaInstagram, FaAt } from "react-icons/fa"; // <-- import styles to be used
 
-import './contact.css';
+import "./contact.css";
 
 export const Contact = () => {
   return (
     <div className="containerContact">
-        <h1 className="title"><span>Contact</span><span>me:</span></h1>
-            <div className="row row-cols-1 socialMedia">
-                <div className="col">
-                <a className="btn btn-primary" href="https://www.linkedin.com/in/cristina-elisabet-shim-b5809ba8/" role="button"><FaLinkedinIn className="icon" /></a>
-                </div>
-                <div className="col">
-                <a className="btn btn-primary" href="https://www.linkedin.com/in/cristina-elisabet-shim-b5809ba8/" role="button"><FaInstagram className="icon" /></a>
-                </div>
-                <div className="col">
-                <a className="btn btn-primary" href="https://www.linkedin.com/in/cristina-elisabet-shim-b5809ba8/" role="button"><FaAt className="icon" /></a>
-                </div>
+      <h2 className="title">
+        <span>Contact</span>
+        <span>me:</span>
+      </h2>
+      <div className="row row-cols-1 socialMedia">
+        <div className="col">
+          <a
+            className="btn btn-primary"
+            href="https://www.linkedin.com/in/cristina-elisabet-shim-b5809ba8/"
+            role="button"
+          >
+            <FaLinkedinIn className="icon" />
+          </a>
         </div>
+        <div className="col">
+          <a
+            className="btn btn-primary"
+            href="https://www.linkedin.com/in/cristina-elisabet-shim-b5809ba8/"
+            role="button"
+          >
+            <FaInstagram className="icon" />
+          </a>
+        </div>
+        <div className="col">
+          <a
+            className="btn btn-primary"
+            href="https://www.linkedin.com/in/cristina-elisabet-shim-b5809ba8/"
+            role="button"
+          >
+            <FaAt className="icon" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
