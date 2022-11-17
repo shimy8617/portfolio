463c612e9941c24bf73276ba0e15ba53e221d75f
shimy8617 <cristinashim@yahoo.com.ar>
2022-11-17 08:52:01 -0300

    dont use this code

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -6,18 +6,16 @@ export const Home = () => {
     return(
         <>
         <Outlet />
-          <div className="presentation">
-          <div className="presentationTitle">
-          <div className="completeName">
-            <h1 className="fadeOut">
-              Cristina
-            </h1>
-            <span className="surname">Shim<span className="fadeIn">y</span></span>
-          </div>
-            <p>React Developer</p>
-            <button type="button">Get in touch</button>
-          </div>
-        </div>
+            <div className="presentationTitle">
+            <div className="completeName">
+              <h1 className="fadeOut">
+                Cristina
+              </h1>
+              <span className="surname">Shim<span className="fadeIn">y</span></span>
+            </div>
+              <p>React Developer</p>
+              <button type="button">Get in touch</button>
+            </div>
         </>
     )
 }
--- src/routes/navigation/navigation.jsx
+++ src/routes/navigation/navigation.jsx
@@ -6,25 +6,35 @@ import "./navigation.css";
 export const Navigation = () => {
   return (
     <Fragment>  
-      <div>
-        <nav className="navigation">
-          <a href="./assets/logo">Logo</a>
-          <ul className="navLinks">
-            <li className="navItems">
-              <a href="/">Home</a>
-            </li>
-            <li className="navItems">
-              <a href="/about">About me</a>
-            </li>
-            <li className="navItems">
-              <a href="/projects">Projects</a>
-            </li>
-            <li className="navItems">
-              <a href="/contact">Contact</a>
-            </li>
-          </ul>
-        </nav>
-      </div>    
+      <nav className="navbar navbar-expand-lg navigation">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">Logo</a>
+          <button className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarSupportedContent" 
+          aria-controls="navbarSupportedContent" 
+          aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="navItems">
+                <a className="nav-link" aria-current="page" href="/">Home</a>
+              </li>
+              <li className="navItems">
+                <a className="nav-link active" aria-current="page" href="/about">About me</a>
+              </li>
+              <li className="navItems">
+                <a className="nav-link" aria-current="page" href="/projects">Projects</a>
+              </li>
+              <li className="navItems">
+                <a className="nav-link" aria-current="page" href="/contact">Contact</a>
+              </li>
+            </ul>
+            </div>
+          </div>
+        </nav>   
       <Outlet />
     </Fragment>
   );
