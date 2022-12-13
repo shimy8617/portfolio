2128cfea798a90a3504805a1b0ca5262e3882391
shimy8617 <cristinashim@yahoo.com.ar>
2022-12-13 14:11:36 -0300

    ofis

--- src/routes/projects/projects.jsx
+++ src/routes/projects/projects.jsx
@@ -12,7 +12,9 @@ export const Projects = () => {
         <div className="row row-cols-2 row-cols-md-2 g-4">
   <div className="col">
     <div className="card h-100">
-      <img src={aypoo} className="card-img-top m-auto w-50" alt="aypoo" />
+      <a href="https://aypoo.com.ar">
+        <img src={aypoo} className="card-img-top m-auto w-50" alt="aypoo" />
+      </a>
       <div className="card-body">
         <h5 className="card-title">Ecommerce</h5>
         <p className="card-text">Wordpress Website for an Ecommerce</p>
@@ -23,7 +25,7 @@ export const Projects = () => {
     <div className="card h-100">
       <img src="..." className="card-img-top" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
+        <h5 className="card-title">Calculator</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
@@ -32,7 +34,7 @@ export const Projects = () => {
     <div className="card h-100">
       <img src="..." className="card-img-top" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
+        <h5 className="card-title">Monsters Rolotex</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
       </div>
     </div>
@@ -41,7 +43,7 @@ export const Projects = () => {
     <div className="card h-100">
       <img src="..." className="card-img-top" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
+        <h5 className="card-title">Rick And Morty</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
