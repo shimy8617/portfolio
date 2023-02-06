3d28a81729025efeb7c47b87107e6a8eaac2a152
shimy8617 <cristinashim@yahoo.com.ar>
2023-02-06 12:09:04 -0300

    css edit

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
-        <Route path="home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
         <Route path="projects" element={<Projects />} />
