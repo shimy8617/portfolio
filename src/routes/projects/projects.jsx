f3545311b9b24595b6625a12c23cf419f53eda79
shimy8617 <cristinashim@yahoo.com.ar>
2023-02-08 15:06:54 -0300

    me

--- src/routes/projects/projects.jsx
+++ src/routes/projects/projects.jsx
@@ -11,32 +11,55 @@ export const Projects = () => {
     <Fragment>
       <div className="container-fluid text-center">
         <h2 className="display-3 fw-bolder">My Projects</h2>
+        
           <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-          Launch demo modal
-        </button>
+          Ecommerce
+          </button>
     
-  
         <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div className="modal-dialog">
             <div className="modal-content">
               <div className="modal-header">
-                <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <h1 className="modal-title fs-5" id="exampleModalLabel">Wordpress Website for an Ecommerce</h1>
                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div className="modal-body">
-                ...
+                <img src={woocommerce} className="card-img-top" alt="aypoo" />
               </div>
               <div className="modal-footer">
                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-primary">Save changes</button>
+                <button type="button" className="btn btn-primary"><a href="https://aypoo.com.ar">Open</a></button>
               </div>
             </div>
           </div>
         </div>
 
+        
+        <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+        Calc App
+      </button>
+  
+      <div className="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel1">Calculator App made with React</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <img src={calc} className="card-img-top" alt="calc" />
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary"><a href="https://react-calc-6b4d1.web.app/">Open</a></button>
+            </div>
+          </div>
+        </div>
+      </div>
 
 
-        <div className="row row-cols-1 row-cols-md-2 g-4">
+
+        {/* <div className="row row-cols-1 row-cols-md-2 g-4">
           <div className="col">
             <div className="card h-100">
               <a href="https://aypoo.com.ar">
@@ -61,7 +84,7 @@ export const Projects = () => {
               </div>
             </div>
           </div>
-          {/* <div className="col">
+          <div className="col">
             <div className="card h-100">
               <img src="..." className="card-img-top" alt="..." />
               <div className="card-body">
@@ -85,8 +108,8 @@ export const Projects = () => {
                 </p>
               </div>
             </div>
-  </div> */}
-        </div>
+  </div> 
+        </div>*/}
       </div>
     </Fragment>
   );
